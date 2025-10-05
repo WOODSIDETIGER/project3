a7a6cf6134b93119e14c3df4706b15b200edd8d8
WOODSIDETIGER <ke2324@yahoo.com>
2025-10-04 21:20:21 -0400

    add marker image and click feature

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ window.initMap = function() {
   const citiFieldLocation = { lat: 40.7571, lng: -73.8458 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
-    center: citiFieldLocation, 
+    center: citiFieldLocation,
   });
-  new google.maps.Marker({
-    position: citiFieldLocation, 
+  const marker = new google.maps.Marker({
+    position: citiFieldLocation,
     map: map,
-    title: "Citi Field",
+    icon: {
+      url: 'images/tearofjoy.png',
+      scaledSize: new google.maps.Size(40, 40)
+    },
+    animation: google.maps.Animation.BOUNCE
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: "<h1>best NYC BASEBALL TEAM HAHA</h1>"
+  });
+  marker.addListener('click', () => {
+    if (infowouter.getMap()) {
+      infowindow.close();
+    } else {
+      infowindow.open(map, marker);
+    }
   });
 };
